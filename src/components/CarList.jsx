b6ae6040ea65d1ec1066f310f4f63b048fd563b0
Mariluz2024 <mluzco29@gmail.com>
2025-01-23 22:48:16 +0100

    Mejorar la bùsqueda por varios atributos del carro

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from "react";
-
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 import CarCard from "./CarCard";
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const CarList = () => {
   const [cars, setCars] = useState([]);
+  const [filteredCars, setFilteredCars] = useState([]);
+  const [searchKey, setSearchKey] = useState("");
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    // Fetch data from the API
     const fetchCars = async () => {
       try {
         const response = await fetch(`${API_BASE_URL}/cars`);
@@ -18,7 +18,8 @@ const CarList = () => {
           throw new Error("Failed to fetch cars");
         }
         const data = await response.json();
-        setCars(data); // Assuming the API returns an array of cars
+        setCars(data);
+        setFilteredCars(data);
       } catch (err) {
         setError(err.message);
       } finally {
@@ -28,6 +29,18 @@ const CarList = () => {
 
     fetchCars();
   }, []);
+
+  const handleSearch = (event) => {
+    const value = event.target.value.toLowerCase();
+    setSearchKey(value);
+    setFilteredCars(
+      cars.filter((car) =>
+        [car.name, car.description, car.price, car.hp, car.model].some(
+          (attribute) => attribute.toString().toLowerCase().includes(value)
+        )
+      )
+    );
+  };
 
   if (loading) {
     return (
@@ -46,10 +59,26 @@ const CarList = () => {
   }
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6 bg-gray-100">
-      {cars.map((car) => (
-        <CarCard key={car._id} car={car} />
-      ))}
+    <div className="p-6 bg-gray-100 min-h-screen">
+      <div className="mb-6">
+        <input
+          type="text"
+          value={searchKey}
+          onChange={handleSearch}
+          placeholder="Search cars by name, description, price, hp, model..."
+          className="w-full p-3 border border-gray-300 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-600"
+        />
+      </div>
+
+      {filteredCars.length > 0 ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {filteredCars.map((car) => (
+            <CarCard key={car._id} car={car} />
+          ))}
+        </div>
+      ) : (
+        <p className="text-center text-gray-600">No cars found!</p>
+      )}
     </div>
   );
 };
