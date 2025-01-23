b8520e889b089a9dc3a8012c564d7c7be6874260
Mariluz2024 <mluzco29@gmail.com>
2025-01-23 22:20:53 +0100

    Navegación a un carro desde favoritos

--- src/components/User/UserFavorite.jsx
+++ src/components/User/UserFavorite.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const FavoriteCarCard = ({ car, favoriteId, onRemove }) => {
+    const navigate = useNavigate();
+    const goToCarDetails = (car) => navigate(`/cars/details`, { state: { car } });
+
     return (
         <div className="flex items-center bg-white border border-gray-200 rounded-lg shadow-md p-4 mb-4">
             {/* Car Image */}
@@ -12,7 +16,7 @@ const FavoriteCarCard = ({ car, favoriteId, onRemove }) => {
 
             {/* Car Details */}
             <div className="ml-4 flex-1">
-                <h2 className="text-lg font-bold text-gray-900">{car.name}</h2>
+                <h2 className="text-lg font-bold text-blue-900" onClick={() => goToCarDetails(car)}>{car.name}</h2>
                 <p className="text-sm text-gray-600 mt-1">
                     ${car.price.toLocaleString()}
                 </p>
