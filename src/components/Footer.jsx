219791b7d39312c779f8d00708db28cbc52e26ae
Mariluz2024 <mluzco29@gmail.com>
2025-01-24 06:19:45 +0100

    Nuevo footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white py-4 text-center">
+      <p className="text-sm">
+        &copy; 2025 - <a href="https://github.com/Mariluz2024">@Mariluz</a>. Todos los derechos reservados.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import CarList from "./CarList";
 import Heading from "./Heading";
+import Footer from "./Footer";
 
 const Home = () => {
   return (
     <div>
       <Heading />
       <CarList />
+      <Footer />
     </div>
   );
 };
